15cf235f21fa6aa2939a865e30c1c3c01a4fe8e6
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-15 21:07:21 -0500

    Create ball class

---
+++ Pong/Ball.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+
+class Ball 
+{
+    private:
+    Vector2f m_Position;
+    RectangleShape m_Shape;
+    float m_speed = 300f
+}
